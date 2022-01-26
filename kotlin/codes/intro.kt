fcec180c0ae78048a3380f2c863ee0e757a636f7
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-01-26 16:18:56 +0000

    Update intro.kt

--- kotlin/codes/intro.kt
+++ kotlin/codes/intro.kt
@@ -26,9 +26,13 @@ fun main(){
   -We can pre-determine the type of a variable using suffix after it name exemple : Byte Int Short Long Double Char String Boolean Float
   - we can add L at the  end of integer declaration for make it long
   - we can add f or F in end of double declaration for make it float
-   
+  -kotlin support binaire and hexadecimal number for write nomber in binaire or hexadecimal we use 01 for binaire 0x for hexadecimal
+      kotlind don't support octal number 
+  
   */
   var a: Long=5// creat Long withe  prefix method
   var b=64L//creat long
   var c=1.84f// creat float
+  var d=0b11001// use binaire number
+  var e=0x1ef // hexadecimal number
 }
