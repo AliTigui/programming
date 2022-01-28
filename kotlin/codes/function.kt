c44f0069fe467d97601838b7df26900eaa7f0693
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-01-28 23:37:10 +0000

    Update function.kt

--- kotlin/codes/function.kt
+++ kotlin/codes/function.kt
@@ -9,6 +9,8 @@
     # in kotlin we par argument by reference but we can't modify them because when we pass them to funtion they arr represented inside it like val variables
     # for make one of the argument optional we add vararg befor it exemple fun name(par1:type,vararg par2:type):retunType{statement } we can put as much as argument
         that we want in vararg parametre and all those argument will be like array if we want pass those argument in array or list format we use * before argument name
+    # we can use name parametre when call function for pass for chack name  the correct argumen exmple  fun1(par="ali",par2=2) but if swe use it we have to use it for all paramatre
+    # functoin parametre defalut value and when we call it we can use it without passing argument exemple fun name(par1:type=val,vararg par2:type=val)
 
 */
 fun main(){
