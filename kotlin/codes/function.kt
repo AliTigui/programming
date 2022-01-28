b782529fa5cb96538889ffc464ffd12b6facde5b
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-01-28 20:52:38 +0000

    Create function.kt

---
+++ kotlin/codes/function.kt
@@ -0,0 +1,21 @@
+/*
+    -function
+    #for creat function we use fun name(par1:type,par2:type):retunType{statement }
+    # every paramatre should have type and we most respect it when call it if function don"t use parametre we can use empty ()
+    # retun type is what functoin return when we call it if return nothing we put it Unit or don't write it at all
+    # for return value we use return
+    # if paramatre is array we use fun name (par:Array<type>)
+    # if function have only 1 line of code we can use fun name (par1:type,par2:type) = a+b or somthing else
+    # in kotlin we par argument by reference but we can't modify them because when we pass them to funtion they arr represented inside it like val variables
+    #
+
+*/
+fun main(){
+
+  var a=5
+  var b=6
+  println(sum(a,b))
+}
+fun sum(p1:Int ,p2:Int):Int{
+ return p1+p2
+}
