08e6e5933c4df25f0b59755ce6b63dfe8bdb3d52
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-01-30 16:10:16 +0000

    Update 3_controlFlow_Loop.kt

--- kotlin/codes/cours/3_controlFlow_Loop.kt
+++ kotlin/codes/cours/3_controlFlow_Loop.kt
@@ -1,1 +1,24 @@
+/*
+-in kotlin there is 4 type of loop
+_ while(condition){statement}  it work until the conditoin inside it become false
+_do{statement}(while) first execute the statement then check the condition if it's true run again else it stop
+_repeat(n){statement} it repeat code inside it n time
+_for(){} it used when we know how much time we want repeat our code we can use it to loop throught in range of number or string or throught list/array element
+exemple: 
+ for(a in 0..9) {println(a)} 
+ for(a in 0 until 10 step 2){ println(a)} step define the step and until mean stop in this value dont get it
+ for(a in 9 downTo 0 step 3){println(a)}  loop down from 9 to 0 the step 3
+ for(a in listName){println(a)} loop throught list element we can also use it with string array map ..
+ we can use continue in loop for skip and don't do any thing if condition is true
+ we can use break to stop loop if condition is true
 
+-condition 
+_if(condition 1){ code 1}else if(condition 2){ code 2} else{ code if all false } we can use if else to return value exemple var a= if(j>5) "big" else "small"
+ we don't have switch case in kotlin we have when statement  when(var){val1 -> statement val2-> statement else->statement} it work like 
+ we can test for range exmple when(var){ in 0..9-> statement else->} we can also return value using when statement like we use in if statement
+
+
+
+
+
+*/
