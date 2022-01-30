42ad0d0e01892274a928635785b972210717349a
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-01-30 13:15:12 +0000

    Update and rename intro.kt to 1_intro.kt

---
+++ kotlin/codes/cours/1_intro.kt
@@ -0,0 +1,11 @@
+/* kotlin Intro and basic
+* kotlin file end with .kt
+* kotlin programme file have main functoin that have all code that we want execut
+* for make line comment we use //
+* for make multiple line comment we use /* */
+* in kotlin we can make multiple line comment inside in other one
+* we declare variable in kotlin using var or val keyword
+* variable with var can be change variable with var can't be change they are constant 
+* kotlin support both functional and orient object programming
+* 
+*/
