7ee0529c9d4a49cee8205af3c14a67f7493c2f8e
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-01-30 14:45:31 +0000

    Update 1_intro.kt

--- kotlin/codes/cours/1_intro.kt
+++ kotlin/codes/cours/1_intro.kt
@@ -13,4 +13,6 @@
   exemple : var a =" 4 + 4 = ${4 + 4 } "
 * comparaison operateur are > < == <= >= != and we have === that mean variable have same reference . !== mean they don't have same reference
 * logical operateur are || (or) && (and) ! (not)
+* Escape sequence: \n \t \b  \r \" \\ \$ ...
+* we can use unicode for write char and string  in kotlin exemple var c='\u0026'
 */
