4dfa4482782ac04ad68fa08573c266b65ca124ed
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-02-01 14:35:28 +0000

    Create regx.kt

---
+++ kotlin/codes/cours/regx.kt
@@ -0,0 +1,5 @@
+/*
+for make regex in kotlin we use Regex("here regular expression") for work with it it better store it in variable
+val exp = Regex("") for chaeck if string match regex we use the method matches in the regex variable exemple exp.matches(str) it return true or false
+
+*/
