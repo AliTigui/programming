ba670c2c654e7edd16abd0d85fd45658245095c4
AliTigui <92318629+AliTigui@users.noreply.github.com>
2022-02-01 15:15:03 +0000

    Create eror.kt

---
+++ kotlin/codes/cours/eror.kt
@@ -0,0 +1,19 @@
+/*
+first code in try run  if non eror we dont run  code in catch but we run code in finally
+if we get eror in statment it stop and then jump to catch run statement inside it then run finally
+finally is optional
+we can add many catch statemtn each one for a special type 
+we can add eror type by using throw if condithion is true throw erore("new eror message ")
+try{
+statement 
+}
+catch(varname:ExceptionType){
+statement 
+// for print only the eror mesage we use varname.message
+
+}
+finaly{
+statement 
+}
+
+*/
