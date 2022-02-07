e64a11413fd3603d34e5054a27ca07a3af496384
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2022-02-07 21:38:01 +0000

    Update 5_function.kt

--- kotlin/codes/cours/5_function.kt
+++ kotlin/codes/cours/5_function.kt
@@ -1,11 +1,15 @@
 /*  
--function
-    #for creat function we use fun name(par1:type,par2:type):retunType{statement }
-    # every paramatre should have type and we most respect it when call it if function don"t use parametre we can use empty ()
-    # retun type is what functoin return when we call it if return nothing we put it Unit or don't write it at all
-    # for return value we use return
-    # if paramatre is array we use fun name (par:Array<type>)
-    # if function have only 1 line of code we can use fun name (par1:type,par2:type) = a+b or somthing else
+-Function:
+we use function to creat block of code that used for specific task it can help use to organise our code and don't repeat
+line of code 
+function have name we can call them with it and it can have argument end return value
+for creat function we use the keywords fun then the name of function then (parametre1:itTyme..parametreN:ittype):return type{
+the function body}
+when we call the function we must respect the type of the pramatre if pramatre string we can't send int to it if function have no paramatre we write empty ()
+return type is the type of what the function will return if it don't return anything we can don't write it or write Unit
+for return value from function we use the keyword return
+if paramatre is array we use fun name (par:Array<type>)
+if function have only 1 line of code we can use fun name (par1:type,par2:type) = a+b 
     # in kotlin we par argument by reference but we can't modify them because when we pass them to funtion they arr represented inside it like val variables
     # for make one of the argument optional we add vararg befor it exemple fun name(par1:type,vararg par2:type):retunType{statement } we can put as much as argument
         that we want in vararg parametre and all those argument will be like array if we want pass those argument in array or list format we use * before argument name
