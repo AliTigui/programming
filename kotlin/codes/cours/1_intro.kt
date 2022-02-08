db9c144247646a20d35262eaa6938ce8e1ddd0e9
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2022-02-08 00:26:24 +0000

    Update 1_intro.kt

--- kotlin/codes/cours/1_intro.kt
+++ kotlin/codes/cours/1_intro.kt
@@ -1,15 +1,9 @@
-/* kotlin Intro and basic
-* kotlin file end with .kt
-* kotlin programme file have main functoin that have all code that we want execut
-* kotlin run in JVM that mean we can put java code in kotlin and it will work  
-* for make line comment we use //
-* for make multiple line comment we use /* */
-* in kotlin we can make multiple line comment inside in other one
+/*
+
+* 
 * we declare variable in kotlin using var or val keyword
 * variable with var can be change variable with var can't be change they are constant 
-* kotlin support both functional and orient object programming
 * Arithmetic operations: + addition - substractoin /division * multiplication % modulo and we have += *= -= /= %= ++a a++ a-- --a 
-* we can print value  using print() or println() deferance is println add line break in the end of the value that we print
 * kotlin support string template that mean we can print or put variable in string using $ before variable and swe can also put equatoin in string using ${equation}
   exemple : var a =" 4 + 4 = ${4 + 4 } "
 * comparaison operateur are > < == <= >= != and we have === that mean variable have same reference . !== mean they don't have same reference
