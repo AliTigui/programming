1552d620e5ce2aa86da036c6bdb10a38d8c95809
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2022-02-14 21:11:32 +0000

    Update 2_types.kt

--- kotlin/codes/cours/2_types.kt
+++ kotlin/codes/cours/2_types.kt
@@ -3,7 +3,6 @@
 
 _for get max or min value of numeric types we use type.MIN_VALUE for min type .MAX_VALUE for max exmple : Int.MIN_VALUE
 _we can format double and float and specific how much degit we want take after print . using "%.DegitNumber".format(doubleVariable) exemple : var a="%.2".format(3.49744)
-_we can writ number with those form too  2e4/2E4 -> (2^4) and 1__4_87_6 ->(14876) 
 
 - null is not type in kotlin that mean if we creat null variable we can't assign other type value for it and also we can't creat type and make it value null
 - for make variable that accept null and other type we use type? exmeple var num:Byte?= null with this we can make it value null  or short
@@ -21,10 +20,7 @@ the 8 weak  byte of our number)
 -we can convert from Char to all except Boolean when we convert we get unicode of the character 
 -we can convert from String to all number if it don't depace their max  or min value we can't convert from String to Char if the stirng is true or false it can be converted 
 to boolean
-#Remarque
 
-_kotlin support binaire and hexadecimal base number we can writ number in hexa by add the prefix 0x and for binaire we use 0b exemple var n=0xf ->(15) but kotlin dont support
-octal number 
 _for remove in neccesary white space in string we use str.trim() for remove for remoe from the end only we use str.trimEnd() and for begin only we use str.trimStart()
 _ str.uppercase() make string ypper case str.lowercase() make string lower case we can use str.toUpperCase str.toLowerCase()  str.toCapitalize()
 _we can replace value in string with other using str.replace("old value","new value")
